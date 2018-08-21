2b518640ce4e85ef60ed4f11e8cd929b42dbd2c4
Unknown <bghahate265@west-mec.org>
2018-08-20 18:27:09 -0700

    Fourth Commit

--- script.js
+++ script.js
@@ -18,19 +18,56 @@ function validateForm(evt) {
     } else {
         evt.returnValue = false;
     }
-    document.getElementById("errorText").innerHTML = "Please check your life's decisions";
-    document.getElementById("errorText").style.display = "block";
+
     validateRequired();
+    if (formValidity) {
+        document.getElementsByTagName("form")[0].submit();
+    }
 }
-// TBC
 
+// function to validate all of the required input fields
 function validateRequired() {
-
+    var selectElements = document.querySelectorAll("#contactinfo input");
+    var errorDiv = document.getElementById("errorText");
+    var elementCount = selectElements.length;
+    var requiredValidity = true;
+    var currentElement = null;
+    try {
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = selectElements[i];
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255, 233, 233)";
+                requiredValidity = false;
+                
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+        if (fieldsetValidity) {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        } else {
+            throw "Please complete all required fields.";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
 }
 
 function createEventListeners() {
-    window.addEventListener("submit", validateForm, false);
+    var form = document.getElementsByTagName("form")[0];
+    if (form.addEventListener) {
+        form.addEventListener("submit", validateForm, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", validateForm);
+    }
 }
 
 // enable load event handlers
-window.addEventListener("load", createEventListeners, false);
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
