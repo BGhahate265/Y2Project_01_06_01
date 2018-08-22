697e113a0b2345fb1ad304b0f855d207a5dd5136
Unknown <bghahate265@west-mec.org>
2018-08-21 18:11:39 -0700

    Fifth Commit
    In Progress

--- script.js
+++ script.js
@@ -1,73 +1,67 @@
 /*
-
-Project 01_06_01
+script.js 
 
 Author: Braddock Ghahate
-Date: 08.15.18
-Filename: script.js
+Date: 15 August 2018
 
+JavaScript to validate index.html
 */
 
 "use strict";
-var formValidity = false;
+var formValidity = true;
 
-//Function to validate the only fieldset/form
+// event handler for submit - validates form
 function validateForm(evt) {
     if (evt.preventDefault) {
         evt.preventDefault();
     } else {
         evt.returnValue = false;
     }
+    formValidity = true;
+    // fuction to validate required fields
+    function validateRequired() {
+        //support variables for validation
+        function validateAddress(fieldsetId) {
+            var inputElements = document.querySelectorAll("#contactinfo input");
+            var errorDiv = document.getElementById("#numErrorText");
+            var fieldsetValidity = true;
+            var elementCount = inputElements.length;
+            var currentElement = null;
+        }
+    }
 
+    //form validation function
     validateRequired();
-    if (formValidity) {
+
+    // validation results
+    if (formValidity === true) {
+        document.getElementById("errorText").innerHTML = "";
+        document.getElementById("errorText").style.display = "none";
         document.getElementsByTagName("form")[0].submit();
+    } else {
+        document.getElementById("errorText").innerHTML = "Please fix the indicated problems and resubmit your personal information";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0, 0);
     }
 }
 
-// function to validate all of the required input fields
-function validateRequired() {
-    var selectElements = document.querySelectorAll("#contactinfo input");
-    var errorDiv = document.getElementById("errorText");
-    var elementCount = selectElements.length;
-    var requiredValidity = true;
-    var currentElement = null;
-    try {
-        for (var i = 0; i < elementCount; i++) {
-            currentElement = selectElements[i];
-            if (currentElement.value === "") {
-                currentElement.style.background = "rgb(255, 233, 233)";
-                requiredValidity = false;
-                
-            } else {
-                currentElement.style.background = "white";
-            }
-        }
-        if (fieldsetValidity) {
-            errorDiv.style.display = "none";
-            errorDiv.innerHTML = "";
-        } else {
-            throw "Please complete all required fields.";
-        }
-    } catch (msg) {
-        errorDiv.style.display = "block";
-        errorDiv.innerHTML = msg;
-        formValidity = false;
-    }
-}
-
+// on load event create necessary event handlers 
 function createEventListeners() {
+    // submit event listener with handler validateForm()
     var form = document.getElementsByTagName("form")[0];
     if (form.addEventListener) {
         form.addEventListener("submit", validateForm, false);
     } else if (form.attachEvent) {
         form.attachEvent("onsubmit", validateForm);
+        // try catch to handle 
     }
+
 }
+//event handler createEventListeners
 
 // enable load event handlers
 if (window.addEventListener) {
     window.addEventListener("load", createEventListeners, false);
 } else if (window.attachEvent) {
-    window.attachEvent("onload", createEventListeners);
+    window.attachEvent("onload", setUpPage);
 }
